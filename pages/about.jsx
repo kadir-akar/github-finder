681b012934e319cdbab4b4019f5effcc6ca1f9aa
Kadir Akar <aakarkadir@gmail.com>
2023-05-22 15:04:16 +0300

    about page update

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,11 +1,25 @@
 import React from "react";
-
+import Link from "next/link";
+import { useState } from "react";
 const about = () => {
   return (
-    <div>
-      <h1>About This App</h1>
-      <p>App to search Github users</p>
-      <p>Version: 1.0.0</p>
+    <div className="mt-5">
+      <h1 className="text-4xl text-center text-gray-100">About this App</h1>
+      <p className="text-center mt-2">
+        This is a Next.js app that uses the Github API to search for users and
+        display their profile information.
+      </p>
+      <p className="text-center">
+        This app was created by{" "}
+        <Link href="/user/kadir-akar" className="text-primary">
+          Kadir Akar
+        </Link>
+        <img
+          src="https://avatars.githubusercontent.com/u/73611059?v=4"
+          alt="kadir-akar"
+          className="rounded-xl m-auto w-80 h-80 mt-10"
+        />
+      </p>
     </div>
   );
 };
